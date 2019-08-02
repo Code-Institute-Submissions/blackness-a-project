814c7b6d13ced609513721b8c7791bb02ab665a2
Ubuntu <ubuntu@ip-172-31-89-25.ec2.internal>
2019-08-02 11:38:56 +0000

    environ var added

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'Blackness'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI',)
 
 @app.route('/')
 def hello():
